b36291a1665714bab394c3e2d2732fc9b39f0f06
Goff-Davis <davispgoff@gmail.com>
2020-05-03 14:44:57 -0400

    Add main scraper

---
+++ watcher.py
@@ -0,0 +1,86 @@
+import datetime
+import praw
+
+SEARCH_STRINGS = ["parts", "repair", "for parts", "for repair", "for parts or repair", "copernic", "kopernic", "copernicus"]
+USER = "MrGamer00"
+SUBREDDIT = "watchexchange"
+
+def main():
+	reddit = create_reddit()
+	destination_user = reddit.redditor(USER)
+	subreddit_to_search = reddit.subreddit(SUBREDDIT)
+
+	search_results = search(subreddit_to_search)
+
+	if found_submissions(search_results):
+		message = create_message(search_results)
+		send_message(message, destination_user)
+
+def create_reddit():
+	return praw.Reddit(
+		"watcher",
+		user_agent="web:watcher:v0.1.0 (by u/MrGamer00)"
+	)
+
+def search(subreddit):
+	results = {}
+
+	for keyword in SEARCH_STRINGS:
+		query = subreddit.search(
+			keyword,
+			sort="new",
+			time_filter="hour"
+		)
+
+		query_results = []
+
+		for submission in query:
+			query_results.append(submission)
+
+		if len(query_results) > 0:
+			results[keyword] = query_results
+
+	return results
+
+def found_submissions(search_results):
+	return bool(search_results)
+
+def create_message(search_results):
+	keywords = list(search_results)
+	category_list = ""
+	links = ""
+	i=0
+
+	for keyword in keywords:
+		if (i < len(keywords) - 1):
+			category_list += "**" + keyword + "**, "
+		else:
+			category_list += "**" + keyword + "** "
+
+		links += "\n\n## " + keyword + "\n"
+
+		for submission in search_results[keyword]:
+			date_created = generate_date_string(submission)
+
+			links += "- [" + submission.title + "](" + submission.permalink + ") (" + date_created + ")\n"
+
+		i += 1
+
+	header = "# New Listings Found\n\nWatches in the categories: " + category_list + "have been found.\n\n"
+	body = "Links to the postings:" + links
+
+	return header + body
+
+def generate_date_string(submission):
+	date = datetime.datetime.fromtimestamp(submission.created_utc)
+
+	return date.strftime("%m-%d-%Y %H:%M")
+
+def send_message(message, user):
+	user.message(
+		"Watches you are looking for have been listed.",
+		message
+	)
+
+if __name__ == "__main__":
+	main()
